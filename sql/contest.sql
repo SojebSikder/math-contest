afc634eec14dbd6dab97b91189572a66b8f0eb2c
sojebsikder <sojebsikder@gmail.com>
2020-07-17 15:13:48 +0600

    master

--- sql/contest.sql
+++ sql/contest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2020 at 10:38 AM
+-- Generation Time: Jul 17, 2020 at 11:11 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
