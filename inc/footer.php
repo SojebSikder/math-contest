32e07b47b68c0765918bcfa433dbd6d1754dc01f
sojebsikder <sojebsikder@gmail.com>
2020-07-18 15:24:12 +0600

    master

--- inc/footer.php
+++ inc/footer.php
@@ -58,6 +58,20 @@ if($aboutus){
 </body>
 </html>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5f12bb4ea45e787d128baa95/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 <?php 
 ob_end_flush();
 ?>
