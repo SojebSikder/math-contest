c1484382298a18f385ba83bee49921b62dba6b40
sojebsikder <sojebsikder@gmail.com>
2020-07-21 13:38:51 +0600

    updated

--- api.php
+++ api.php
@@ -1,55 +1,64 @@
 <?php
 include "config/conn.php";
 include "lib/Database.php";
+include "helpers/Format.php";
 
+class API{
+    private $db;
+    private $format;
+    private $address;
 
-
-if(isset($_REQUEST['id'])){
-    if($_REQUEST['id'] != null){
-
-        class API{
-            function  Select($id){
-                $db = new Database();
-                $users = array();
-                $data = $db->select("SELECT * FROM register WHERE id=$id");
-
-                if($data){
-
-                    while ($output = $data->fetch_assoc()) {
-                        $users = array(
-                            'id' => $output['id'],
-                            'user_name' => $output['user_name'],
-                            'user_email' => $output['user_email']
+    function __construct(){
+        $this->db = new Database();
+        $this->format = new Format();
         
-                        );
-                    }
-                    return json_encode($users);
-                }else{
-                    echo "perameter wrong";
-                }
-            }
-        }
-
+        $this->address = "http://localhost/math-contest/";
     }
-    
-}else{
 
-    class API{
-        function  Select(){
-            $db = new Database();
-            $users = array();
-            $data = $db->select("SELECT * FROM register");
+    public function  Select($id){
+        $id = $this->format->Stext($id);
+        $users = array();
+        $data = $this->db->select("SELECT * FROM register WHERE id=$id");
+
+        if($data){
 
             while ($output = $data->fetch_assoc()) {
                 $users = array(
                     'id' => $output['id'],
                     'user_name' => $output['user_name'],
                     'user_email' => $output['user_email']
+
                 );
             }
             return json_encode($users);
+        }else{
+            echo "perameter wrong";
         }
-    }
+    } // end function
+
+
+    public function  fetchProblem($id){
+        $id = $this->format->Stext($id);
+        $users = array();
+        $data = $this->db->select("SELECT * FROM post WHERE id=$id");
+
+        if($data){
+
+            while ($output = $data->fetch_assoc()) {
+                $users = array(
+                    'id' => $output['id'],
+                    'post_title' => $output['post_title'],
+                    'post_content' => $output['post_content'],
+                    'post_image' => $this->address.$output['post_image'],
+                    'post_author' => $output['post_author']
+                );
+            }
+            return json_encode($users);
+        }else{
+            echo "perameter wrong";
+        }
+    } // end function
+
 
 }
 
@@ -69,11 +78,21 @@ if(isset($_REQUEST['id']) != null){
     }
 }
 
-else{
-    $API = new API();
-    header('Content-Type: application/json');
-    echo $API->Select();
+
+if(isset($_REQUEST['fetchmath']) != null){
+
+    if($_REQUEST['fetchmath'] != null){
+    
+        $id = $_REQUEST['fetchmath'];
+        $API = new API();
+        header('Content-Type: application/json');
+        echo $API->fetchProblem($id);
+    }
+    else{
+        echo "something went wrong";
+    }
 }
+
 
 
 ?>
